0d837bf74b9e61c24119b0e08b3bacca905db7b4
Thomas Marshall <thomas@thomasmarshall.com>
2020-12-07 11:38:09 +0000

    Add example current_user

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -10,7 +10,7 @@ class GraphqlController < ApplicationController
     operation_name = params[:operationName]
     context = {
       # Query context goes here, for example:
-      # current_user: current_user,
+      current_user: current_user,
     }
     result = ExampleSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
@@ -20,6 +20,13 @@ class GraphqlController < ApplicationController
   end
 
   private
+
+  def current_user
+    @current_user ||= User.find_or_create_by!(
+      name: 'Bart Simpson',
+      email: 'bart.simpson@example.com'
+    )
+  end
 
   # Handle variables in form data, JSON body, or a blank value
   def prepare_variables(variables_param)
